35c31adfcea75d32a7494c9b9994288ca4f79030
Brandon Holt <bholt@cs.washington.edu>
2014-04-14 23:19:39 -0700

    igor script for running pagerank experiments

---
+++ igor_pagerank.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+require 'igor'
+
+Igor do
+  
+  sbatch_flags.push *%w[
+    --account=pal
+    --partition=pal
+    --time=1:00:00
+  ]
+  
+  database '~/osdi.sqlite', :pagerank
+  
+  path = File.expand_path(File.dirname(__FILE__)) + "/release/toolkits/graph_analytics"
+  
+  command "source ~/graphlab.env.sh; mpirun #{path}/pagerank --graph /pic/projects/grappa/twitter/bintsv4/twitter-all.bintsv4 --format bintsv4 --graph_opts='%{graph_opts}' --iterations=%{iterations}"
+  
+  params {
+    dataset 'twitter'
+    version 'graphlab'
+    ppn 1
+    nnode 16
+    graph_opts 'ingress=oblivious,usehash=1'
+    iterations 0
+  }
+  
+  parser {|out|
+    h = {}
+    out.gsub(/Finished Running engine in (#{REG_NUM}) seconds\./){ h[:time] = $~[1] }    
+    out.gsub(/nverts:\s+(\d+).*?
+              nedges:\s+(\d+).*?
+              nreplicas:\s+(\d+).*?
+              replication factor: #{REG_NUM}/xm){
+      h[:nverts] = $~[1]
+      h[:nedges] = $~[2]
+      h[:nreplicas] = $~[3]
+      h[:replication_factor] = $~[4]
+    }
+    return h
+  }
+  
+  interact
+  
+end
